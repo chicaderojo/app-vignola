a24818094743cce5555b5016a430dedf7f8144c2
franciscoiatolosa <francisco.iatolosa@gmail.com>
2026-01-29 14:35:25 -0300

    error icon

--- vite.config.ts
+++ vite.config.ts
@@ -18,14 +18,9 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: '/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: '/vite.svg',
+            sizes: '192x192 512x512',
+            type: 'image/svg+xml'
           }
         ]
       },
