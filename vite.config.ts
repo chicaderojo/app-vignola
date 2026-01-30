44a2311018dc04dc6ee1c490d29df5b47f75e764
franciscoiatolosa <francisco.iatolosa@gmail.com>
2026-01-30 02:15:35 -0300

    deploy3

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3 MB
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         runtimeCaching: [
           {
